3c81dcb0b6699bb26d60266eca8ec3920819c8bf
kevinchen19888 <kevinchen19888@163.com>
2020-06-21 23:19:30 +0800

    basic python class use demo

--- basic/python_class.py
+++ basic/python_class.py
@@ -1,4 +1,3 @@
-# -*- coding: UTF-8 -*-
 """类的创建与使用"""
 
 
@@ -6,9 +5,9 @@ class Employee:
     """员工基类"""
     empCount = 0
 
-    def __init__(self, name, salay):
+    def __init__(self, name, salary):
         self.name = name
-        self.salay = salay
+        self.salary = salary
         Employee.empCount += 1
         print("Employee初始化")
 
@@ -16,16 +15,21 @@ class Employee:
         print("员工数量:", Employee.empCount)
 
     def emp_salary(self):
-        print("薪水:", self.salay)
+        print("薪水:", self.salary)
 
 
 emp1 = Employee("kevin", 100)
 # emp2 = Employee("kevin", 100)
-# print(emp1.salay)
-print(emp1.__dict__)  # 类的所有属性与值得字典
-print(emp1.__doc__)  # 类的文档字符串
-print(Employee.__name__)
-print(Employee.__module__)
+# print(emp1.salary)
+print(getattr(emp1, 'name'))
+setattr(emp1, 'name', "james")  # 设置对象属性值
+print(getattr(emp1, 'name'))  # 获取对象属性值
+
+
+# print(emp1.__dict__)  # 类的所有属性与值得字典
+# print(emp1.__doc__)  # 类的文档字符串
+# print(Employee.__name__)
+# print(Employee.__module__)
 
 
 # print(emp1.empCount)
--- basic/python_io.py
+++ basic/python_io.py
@@ -1,4 +1,3 @@
-# --coding:utf-8
 import os
 
 # input = input("请输入:")
